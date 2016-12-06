a06c88f8c1e9cd32cbd8b148e57e46dc1b92db9c
cxyliuyu <cxyliuyu@sina.com>
2016-12-06 11:16:57 +0800

    init commit

--- TestScrapy/TestScrapy/spiders/dmoz_spider.py
+++ TestScrapy/TestScrapy/spiders/dmoz_spider.py
@@ -7,7 +7,6 @@ class DmozSpider(BaseSpider):
     allowed_domains = ["dmoz.org"]
     start_urls = [
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
 
     def parse(self, response):
