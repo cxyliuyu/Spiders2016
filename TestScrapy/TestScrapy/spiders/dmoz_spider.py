4ce22ba23566de3e277239b29972371ed9ad9d5f
cxyliuyu <cxyliuyu@sina.com>
2016-12-08 22:24:51 +0800

    init commit

--- TestScrapy/TestScrapy/spiders/dmoz_spider.py
+++ TestScrapy/TestScrapy/spiders/dmoz_spider.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-from scrapy.spider import BaseSpider
+import scrapy
 from ..items import DmozItem
 
-class DmozSpider(BaseSpider):
+class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/"
     ]
 
     def parse(self, response):
-        for sel in response.xpath('//ul/li'):
+        for sel in response.xpath('//div[@class="site-item"]'):
             item = DmozItem()
-            item['title'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
+            item['title'] = sel.xpath('//div[@class="site-title"]/text()').extract()
+            item['link'] = sel.xpath('div[@class="title-and-desc"]/a/@href').extract()
+            item['desc'] = sel.xpath('//div[@class="site-descr"]/text()').extract()
             yield item
