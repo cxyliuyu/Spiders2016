8a985ad45768e5c5aa8e0341f3ac94762437df02
cxyliuyu <cxyliuyu@sina.com>
2016-12-16 16:51:29 +0800

    init commit

--- TestScrapy/TestScrapy/spiders/dmoz_spider.py
+++ TestScrapy/TestScrapy/spiders/dmoz_spider.py
@@ -16,5 +16,4 @@ class DmozSpider(scrapy.Spider):
             item['link'] = sel.xpath('div[@class="title-and-desc"]/a/@href').extract()
             item['desc'] = sel.xpath('//div[@class="site-descr"]/text()').extract()
 
-
             yield item
